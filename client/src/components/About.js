cc67cf4cabb61bb4835eecd78c8c67ae3bfabc4b
Jack Bird <Jbird43@uwo.ca>
2024-05-11 22:37:58 -0400

    Added basking react-server communication

---
+++ client/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This section will describe my background, experience, and skills.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ client/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Information</h1>
+      <p>Email: example@example.com</p>
+      <p>LinkedIn: [Your LinkedIn]</p>
+    </div>
+  );
+}
+
+export default Contact;
